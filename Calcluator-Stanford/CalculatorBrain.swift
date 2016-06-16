db9079bc337c3e4d5d12908b2c14142728e43fe2
Ben Smith <benjaminsmith1981@gmail.com>
2016-06-16 10:08:48 +0200

    brain an view

---
+++ Calcluator-Stanford/CalculatorBrain.swift
@@ -0,0 +1,24 @@
+//
+//  CalculatorBrain.swift
+//  Calcluator-Stanford
+//
+//  Created by Ben Smith on 16/06/16.
+//  Copyright © 2016 Ben Smith. All rights reserved.
+//
+
+import Foundation
+class CalculatorBrain {
+    func setOperand(operand: Double) {
+        
+    }
+    
+    func performOperation(symbol: String) {
+        
+    }
+    
+    var result: Double {
+        get {
+            return 0
+        }
+    }
+}
